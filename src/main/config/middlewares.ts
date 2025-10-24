46ac9d2829ea71bb7e95f35a73c897fe400b26fb
João Pedro Monteiro Pereira <devpedromonteiro@gmail.com>
2025-10-24 12:00:54 -0300

    fix(middlewares): DJ-15 resolve formatting issue in applyMiddlewares

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,7 @@
 import express, { Express } from "express";
 import helmet from "helmet";
 
-export const applyMiddlewares = (app: Express): void  => {
+export const applyMiddlewares = (app: Express): void => {
     app.use(express.json());
     app.use(helmet());
 };
