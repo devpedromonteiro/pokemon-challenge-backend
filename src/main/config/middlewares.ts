3ff206504b10a7ace5ec8ed5639c2742b43d6423
João Pedro Monteiro Pereira <devpedromonteiro@gmail.com>
2025-10-24 12:29:11 -0300

    fix(middlewares): DJ-15 set default response type to JSON

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -4,5 +4,9 @@ import helmet from "helmet";
 export const applyMiddlewares = (app: Express): void => {
     app.use(express.json());
     app.use(helmet());
+    app.use((req, res, next) => {
+        res.type("json");
+        next();
+    });
 };
 
