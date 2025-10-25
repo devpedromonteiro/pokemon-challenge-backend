9d783f86e6b4eb04d0b2fb1f0ae2c583e5b5b489
João Pedro Monteiro Pereira <devpedromonteiro@gmail.com>
2025-10-25 16:16:20 -0300

    style(linting): DJ-29 format code and improve readability
    This commit addresses various code formatting and linting issues across the
    project to ensure better readability and maintainability.
    
    Changes include:
    - Utilizing TypeScript's type imports for more concise code in
      `middlewares.ts` and `error-handler.ts`.
    - Standardizing the use of underscores in unused parameters
      (e.g., `_req`) across middleware functions to indicate their
      intended non-use, improving code clarity.
    - Adding spaces and line breaks for better formatting, particularly
      in the logging statement within `server.ts`, ensuring consistency in
      code style.
    - Fixing a missing newline at the end of the file in `get-error-message.ts`.
    
    These improvements not only comply with the chosen linter configurations
    but also make the overall codebase easier to read for new developers
    joining the project. This aligns with our ongoing efforts to enhance
    the development experience and maintain high code quality standards.

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export const env = {
     port: process.env.PORT || 3000,
     nodeEnv: process.env.NODE_ENV || "development",
-    debug: process.env.DEBUG  === "true",
+    debug: process.env.DEBUG === "true",
 };
