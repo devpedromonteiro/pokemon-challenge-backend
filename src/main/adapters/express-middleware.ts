ebc5934ba0a9374442d5767768a470c5b199eb26
João Pedro Monteiro Pereira <devpedromonteiro@gmail.com>
2025-10-27 13:58:15 -0300

    feat(express): DJ-16 add adapters for express middleware and router
    This commit introduces two new adapters for integrating middleware
    and controller logic with Express.js. The `adaptExpressMiddleware`
    function adapts a custom middleware to handle requests, ensuring that
    valid entries from the middleware's response are added to `req.locals`,
    while handling any errors gracefully by sending appropriate JSON error
    responses.
    
    Similarly, the `adaptExpressRoute` function is designed to integrate
    controllers with Express routes. It merges request body data and `req.locals`
    when invoking the controller, and it returns either the response data or
    an error message based on the status code of the controller's response.
    
    The addition of these adapters allows for a more structured way to handle
    HTTP requests and responses in an Express application, leading to cleaner
    and more maintainable code. This change aims to improve the compatibility
    and usability of our application with Express's architecture, following
    best practices for middleware and routing.

---
+++ src/main/adapters/express-middleware.ts
@@ -0,0 +1,18 @@
+import type { RequestHandler } from "express";
+import type { Middleware } from "@/application/middlewares";
+
+type Adapter = (middleware: Middleware) => RequestHandler;
+
+export const adaptExpressMiddleware: Adapter = (middleware) => async (req, res, next) => {
+    const { statusCode, data } = await middleware.handle({
+        ...req.headers,
+    });
+
+    if (statusCode === 200) {
+        const validEntries = Object.entries(data).filter(([, value]) => value);
+        req.locals = { ...req.locals, ...Object.fromEntries(validEntries) };
+        next();
+    } else {
+        res.status(statusCode).json({ error: data.message });
+    }
+};
