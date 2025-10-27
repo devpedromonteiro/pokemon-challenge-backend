f5a0639b1a708b2317927853b1622e9602116a3b
João Pedro Monteiro Pereira <devpedromonteiro@gmail.com>
2025-10-27 20:17:00 -0300

    test(validation): DJ-30 add unit tests for ValidationComposite
    This commit introduces a new test suite for the
    ValidationComposite class, aimed at ensuring the
    correct behavior of the validation logic used
    within the application.
    
    The added tests validate various scenarios:
    1. When all validators return `undefined`, the
       composite should also return `undefined`, indicating
       that there are no validation errors.
    2. When one or more validators return an error,
       the composite should return the first encountered
       error as expected.
    
    These tests are crucial for verifying the functionality
    of our validation mechanism, enhancing the reliability
    of the application by catching potential issues early
    in the development cycle. Furthermore, with these
    tests in place, any future changes to the validation
    logic can be confidently made, knowing that existing
    functionality is protected.

---
+++ tests/application/validation/composite.spec.ts
@@ -0,0 +1,44 @@
+import { type MockProxy, mock } from "jest-mock-extended";
+import { ValidationComposite, type Validator } from "@/application/validation";
+
+describe("ValidationComposite", () => {
+    let sut: ValidationComposite;
+    let validator1: MockProxy<Validator>;
+    let validator2: MockProxy<Validator>;
+    let validators: Validator[];
+
+    beforeAll(() => {
+        validator1 = mock<Validator>();
+        validator1.validate.mockReturnValue(undefined);
+        validator2 = mock<Validator>();
+        validator2.validate.mockReturnValue(undefined);
+        validators = [validator1, validator2];
+    });
+
+    beforeEach(() => {
+        sut = new ValidationComposite(validators);
+    });
+
+    it("should return undefined if all Validators return undefined", () => {
+        const error = sut.validate();
+
+        expect(error).toBeUndefined();
+    });
+
+    it("should return the first error", () => {
+        validator1.validate.mockReturnValueOnce(new Error("error_1"));
+        validator2.validate.mockReturnValueOnce(new Error("error_2"));
+
+        const error = sut.validate();
+
+        expect(error).toEqual(new Error("error_1"));
+    });
+
+    it("should return error if any Validator fails", () => {
+        validator2.validate.mockReturnValueOnce(new Error("error_2"));
+
+        const error = sut.validate();
+
+        expect(error).toEqual(new Error("error_2"));
+    });
+});
