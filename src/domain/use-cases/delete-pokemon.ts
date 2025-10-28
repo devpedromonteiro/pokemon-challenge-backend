7784704c6f185ce771d6bc5ce23396547d6227b0
João Pedro Monteiro Pereira <devpedromonteiro@gmail.com>
2025-10-27 22:33:57 -0300

    feat(use-cases): DJ-30 add deletePokemon use case implementation
    This commit introduces the `deletePokemon` use case, which allows
    users to remove a Pokémon from the repository by its unique ID.
    The implementation includes a setup function that takes a
    `PokemonRepository` as a parameter, ensuring a clean separation
    of concerns and enabling easier testing.
    
    The new use case checks if the Pokémon exists before attempting
    to delete it. If the Pokémon is not found, an error is thrown,
    providing feedback on the operation's success or failure. This
    validation step is crucial for maintaining data integrity and
    preventing unexpected behavior when trying to delete
    non-existent resources.
    
    By incorporating this new functionality, we enhance the CRUD
    capabilities of the Pokémon management system, providing a
    complete set of operations for managing Pokémon within the
    application.

---
+++ src/domain/use-cases/delete-pokemon.ts
@@ -0,0 +1,24 @@
+import type { PokemonRepository } from "@/domain/contracts/repos";
+
+type Setup = (pokemonRepository: PokemonRepository) => DeletePokemon;
+type Input = { id: number };
+type Output = void;
+
+export type DeletePokemon = (input: Input) => Promise<Output>;
+
+/**
+ * Setup function for DeletePokemon use case
+ * @param pokemonRepository - Pokemon repository instance
+ * @returns DeletePokemon use case function
+ */
+export const setupDeletePokemon: Setup =
+    (pokemonRepository) =>
+    async ({ id }) => {
+        const pokemon = await pokemonRepository.loadById(id);
+
+        if (!pokemon) {
+            throw new Error("Pokemon not found");
+        }
+
+        await pokemonRepository.deleteById(id);
+    };
